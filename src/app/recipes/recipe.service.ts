107c5093c9dd0c72c6f6e634d164e89bce72c382
bshearrer <bshearr@outlook.com>
2022-05-30 12:38:33 -0500

    Services Module finished

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,16 +1,33 @@
 import {EventEmitter, Injectable} from "@angular/core";
 import {Recipe} from "./recipe.model";
+import {Ingredient} from "../shared/ingredient.model";
+import {ShoppingListService} from "../shopping-list/shopping-list.service";
 
 @Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('Test Recipe','This is a test recipe','https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_960_720.png'),
-    new Recipe('Second Recipe','This is a test recipe','https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_960_720.png'),
+    new Recipe('Cheeseburger','Double Bacon Cheeseburger','https://cdn-icons-png.flaticon.com/512/198/198416.png',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('Cheese', 1),
+      ]),
+    new Recipe('Hot Dog','New York Style Hot Dog','https://cdn-icons-png.flaticon.com/512/198/198474.png',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 2),
+      ]),
   ];
+
+  constructor(private shoppingListService: ShoppingListService) {
+  }
 
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients);
   }
 }
